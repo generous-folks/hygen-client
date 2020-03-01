f6148c37db6baf9107353c0aa3bc46676d290098
p-huyghe <phuyghe.externe@m6.fr>
2020-03-01 01:41:58 +0100

    first commit

---
+++ src/components/fileview.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Container from '@material-ui/core/Container';
+
+export const FileView = () => {
+  return (
+    <Container>
+      <p>Hello file view</p>
+    </Container>
+  );
+};
